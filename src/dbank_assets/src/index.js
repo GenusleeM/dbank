1de8ef1d7ebf96172d118379541d476313fe2b64
Genuslee Mapedze <genuslee.developer@gmail.com>
2022-04-14 15:24:42 +0200

    1st blockchain app

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,18 @@
-import { dbank } from "../../declarations/dbank";
+import {dbank} from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async ()=>{
+  // console.log("finseh lo");
+  const currentAmount =await dbank.checkBalance();
+  document.getElementById("value").innerText = currentAmount.toFixed(2);
 });
+
+document.querySelector("form").addEventListener("submit", async (event)=>{
+  // console.log("Submited");
+  const inputAmount =parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdraw-amount").value);
+
+
+  await dbank.topUp(inputAmount);
+  const currentAmount =await dbank.checkBalance();
+  document.getElementById("value").innerText = currentAmount.toFixed(2);
+})
